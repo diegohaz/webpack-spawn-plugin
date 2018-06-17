d14a1bc04dfc0d7fc35cf8855394b5c63f4b60a9
Xavier Cambar <xcambar@gmail.com>
2018-06-17 21:56:36 +0200

    Adds a 'persistent' option to maintain the spawned process (#15)

--- src/index.js
+++ src/index.js
@@ -5,25 +5,39 @@ import fkill from 'fkill'
 type Options = {
   when: string,
   stdio: string,
+  persistent: boolean
 }
 
 class SpawnPlugin {
   pid: any
   when: string
+  persistent: boolean
   args: any[]
 
   constructor(
     command: string,
     args: any[] = [],
-    { when = 'done', stdio = 'inherit', ...options }: Options = {}
+    {
+      when = 'done',
+      stdio = 'inherit',
+      persistent = false,
+      ...options
+    }: Options = {}
   ) {
     this.when = when
+    this.persistent = persistent
     this.args = [command, args, { stdio, ...options }]
   }
 
   apply(compiler: any) {
     compiler.hooks[this.when].tap({ name: 'webpack-spawn-plugin' }, () => {
-      const promise = this.pid ? fkill(this.pid) : Promise.resolve()
+      let promise = Promise.resolve()
+      if (this.pid) {
+        if (this.persistent) {
+          return
+        }
+        promise = fkill(this.pid)
+      }
       const doSpawn = () => {
         const server = spawn(...this.args)
         this.pid = server.pid
